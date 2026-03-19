1823a12ba60069a6260c453127d029d1e0eab764
Anqi <anqiyang1270@alumnes.ilerna.com>
2026-03-19 19:20:34 +0100

    pagina completo

--- fitvida-app/src/app/app.ts
+++ fitvida-app/src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// 1. Import custom components
+import { HeroCard } from './hero-card/hero-card';
+
+// 2. Import Angular Material modul
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  // 3. Activate these modules in the imports array.
+  imports: [
+    CommonModule,
+    HeroCard,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('fitvida-app');
 }
